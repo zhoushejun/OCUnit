926a49870435c277c2e7902bdb2fd1025440df30
zhoushejun <965678322Zhou>
2014-12-28 20:04:19 +0800

    Initial Commit

---
+++ OCUnit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCUnit
+//
+//  Created by zhoushejun on 14-12-28.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCUnit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCUnit
+//
+//  Created by zhoushejun on 14-12-28.
+//  Copyright (c) 2014年 shejun.zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
